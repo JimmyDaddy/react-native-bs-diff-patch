3e4ec78d211a1a0530b8e88421cf4417e07ac80e
JimmyDaddy <heyjimmygo@gmail.com>
2024-01-20 14:15:41 +0800

    feat: ios project settings

--- android/src/main/java/com/jimmydaddy/bsdiffpatch/BsDiffPatchModule.kt
+++ android/src/main/java/com/jimmydaddy/bsdiffpatch/BsDiffPatchModule.kt
@@ -10,98 +10,98 @@ import java.io.File
 
 @ReactModule(name = BsDiffPatchModule.NAME)
 class BsDiffPatchModule(reactContext: ReactApplicationContext?) :
-    ReactContextBaseJavaModule(reactContext) {
-    override fun getName(): String {
-        return NAME
+  ReactContextBaseJavaModule(reactContext) {
+  override fun getName(): String {
+    return NAME
+  }
+
+  private fun getFileDir(dir: String): String {
+    if (dir.startsWith("file://")) {
+      return dir.substring(7)
+    }
+    return dir
+  }
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun patch(oldFile: String?, newFile: String?, patchFile: String?, promise: Promise) {
+    if (oldFile == null || newFile == null || patchFile == null || oldFile.isEmpty() || newFile.isEmpty() || patchFile.isEmpty()) {
+      promise.reject("error", "oldFile, newFile, patchFile can not be null or empty")
+      return
+    }
+    if (oldFile == newFile || oldFile == patchFile || newFile == patchFile) {
+      promise.reject("error", "oldFile, newFile, patchFile can not be the same")
+      return
+    }
+    val oldFileObj = File(getFileDir(oldFile))
+    if (!oldFileObj.exists()) {
+      promise.reject("error", "oldFile: $oldFile not exist")
+      return
+    }
+    val patchFileObj = File(getFileDir(patchFile))
+    if (!patchFileObj.exists()) {
+      promise.reject("error", "patchFile: $patchFile not exist")
+      return
+    }
+    val newFileObj = File(getFileDir(newFile))
+    if (newFileObj.exists()) {
+      promise.reject("error", "newFile: $newFile already exist")
+      return
+    }
+    try {
+      val result = bsPatchFile(oldFileObj.absolutePath, newFileObj.absolutePath, patchFileObj.absolutePath)
+      promise.resolve(result)
+    } catch (e: Exception) {
+      promise.reject("error", e.message)
+    }
+  }
+
+  @ReactMethod
+  fun diff(oldFile: String?, newFile: String?, patchFile: String?, promise: Promise) {
+    if (oldFile == null || newFile == null || patchFile == null || oldFile.isEmpty() || newFile.isEmpty() || patchFile.isEmpty()) {
+      promise.reject("error", "oldFile, newFile, patchFile can not be null or empty")
+      return
+    }
+    if (oldFile == newFile || oldFile == patchFile || newFile == patchFile) {
+      promise.reject("error", "oldFile, newFile, patchFile can not be the same")
+      return
+    }
+    val oldFileObj = File(getFileDir(oldFile))
+    if (!oldFileObj.exists()) {
+      promise.reject("error", "oldFile: $oldFile not exist")
+      return
+    }
+    val newFileObj = File(getFileDir(newFile))
+    if (!newFileObj.exists()) {
+      promise.reject("error", "newFile: $newFile not exist")
+      return
+    }
+    val patchFileObj = File(getFileDir(patchFile))
+    if (patchFileObj.exists()) {
+      promise.reject("error", "patchFile: $patchFile already exist")
+      return
+    }
+    try {
+      val result = bsDiffFile(oldFileObj.absolutePath, newFileObj.absolutePath, patchFileObj.absolutePath)
+      promise.resolve(result)
+    } catch (e: Exception) {
+      promise.reject("error", e.message)
+    }
+  }
+
+  companion object {
+    const val NAME = "BsDiffPatch"
+
+    init {
+      System.loadLibrary("react-native-bs-diff-patch")
     }
 
-    fun getFileDir(dir: String): String {
-        if (dir.startsWith("file://")) {
-            return dir.substring(7)
-        }
-        return dir
-    }
+    // Used to load the 'native-lib' library on application startup.
+    // get new file from old file and patch file
+    private external fun bsPatchFile(oldFile: String, newFile: String, patchFile: String): Int
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun patch(oldFile: String?, newFile: String?, patchFile: String?, promise: Promise) {
-        if (oldFile == null || newFile == null || patchFile == null || oldFile.isEmpty() || newFile.isEmpty() || patchFile.isEmpty()) {
-            promise.reject("error", "oldFile, newFile, patchFile can not be null or empty")
-            return
-        }
-        if (oldFile == newFile || oldFile == patchFile || newFile == patchFile) {
-            promise.reject("error", "oldFile, newFile, patchFile can not be the same")
-            return
-        }
-        val oldFileObj = File(getFileDir(oldFile))
-        if (!oldFileObj.exists()) {
-            promise.reject("error", "oldFile not exist")
-            return
-        }
-        val patchFileObj = File(getFileDir(patchFile))
-        if (!patchFileObj.exists()) {
-            promise.reject("error", "patchFile not exist")
-            return
-        }
-        val newFileObj = File(getFileDir(newFile))
-        if (newFileObj.exists()) {
-            promise.reject("error", "newFile already exist")
-            return
-        }
-        try {
-            val result = bsPatchFile(oldFileObj.absolutePath, newFileObj.absolutePath, patchFileObj.absolutePath)
-            promise.resolve(result)
-        } catch (e: Exception) {
-            promise.reject("error", e.message)
-        }
-    }
-
-    @ReactMethod
-    fun diff(oldFile: String?, newFile: String?, patchFile: String?, promise: Promise) {
-        if (oldFile == null || newFile == null || patchFile == null || oldFile.isEmpty() || newFile.isEmpty() || patchFile.isEmpty()) {
-            promise.reject("error", "oldFile, newFile, patchFile can not be null or empty")
-            return
-        }
-        if (oldFile == newFile || oldFile == patchFile || newFile == patchFile) {
-            promise.reject("error", "oldFile, newFile, patchFile can not be the same")
-            return
-        }
-        val oldFileObj = File(getFileDir(oldFile))
-        if (!oldFileObj.exists()) {
-            promise.reject("error", "oldFile not exist")
-            return
-        }
-        val newFileObj = File(getFileDir(newFile))
-        if (!newFileObj.exists()) {
-            promise.reject("error", "newFile not exist")
-            return
-        }
-        val patchFileObj = File(getFileDir(patchFile))
-        if (patchFileObj.exists()) {
-            promise.reject("error", "patchFile already exist")
-            return
-        }
-        try {
-            val result = bsDiffFile(oldFileObj.absolutePath, newFileObj.absolutePath, patchFileObj.absolutePath)
-            promise.resolve(result)
-        } catch (e: Exception) {
-            promise.reject("error", e.message)
-        }
-    }
-
-    companion object {
-        const val NAME = "BsDiffPatch"
-
-        init {
-            System.loadLibrary("react-native-bs-diff-patch")
-        }
-
-        // Used to load the 'native-lib' library on application startup.
-        // get new file from old file and patch file
-        private external fun bsPatchFile(oldFile: String, newFile: String, patchFile: String): Int
-
-        // generate patch file
-        private external fun bsDiffFile(oldFile: String, newFile: String, patchFile: String): Int
-    }
+    // generate patch file
+    private external fun bsDiffFile(oldFile: String, newFile: String, patchFile: String): Int
+  }
 }
--- ios/BsDiffPatch.mm
+++ ios/BsDiffPatch.mm
@@ -5,15 +5,88 @@ RCT_EXPORT_MODULE()
 
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
-RCT_EXPORT_METHOD(multiply:(double)a
-                  b:(double)b
+RCT_EXPORT_METHOD(patch:(NSString*) oldFile
+                  newFile:(NSString*) newFile
+                  patchFile:(NSString*) patchFile
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
-    NSNumber *result = @(bsdiffpatch::multiply(a, b));
+    if (oldFile == nil || oldFile.length == 0 || newFile == nil || newFile.length == 0 || patchFile == nil || patchFile.length == 0) {
+        NSString *message = @"oldFile, newFile, and patchFile should not be nil or empty";
+        reject(@"EINVAL", message, nil);
+        return;
+    }
+    if ([oldFile isEqualToString:newFile] || [oldFile isEqualToString:patchFile] || [newFile isEqualToString:patchFile]) {
+        NSString *message = @"oldFile, newFile, and patchFile should not be the same";
+        reject(@"EINVAL", message, nil);
+        return;
+    }
 
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:oldFile]) {
+        NSString *message = [NSString stringWithFormat:@"oldFile: %@! does not exist", oldFile];
+        reject(@"ENOENT", message, nil);
+        return;
+    }
+    if ([fileManager fileExistsAtPath:newFile]) {
+        NSString *message = [NSString stringWithFormat:@"newFile: %@! already exists", newFile];
+        reject(@"ENOENT", message, nil);
+        return;
+    }
+    if (![fileManager fileExistsAtPath:patchFile]) {
+        NSString *message = [NSString stringWithFormat:@"patchFile: %@! does not exist", newFile];
+        reject(@"ENOENT", message, nil);
+        return;
+    }
+
+    const char *oldFileCString = [oldFile UTF8String];
+    const char *newFileCString = [newFile UTF8String];
+    const char *patchFileCString = [patchFile UTF8String];
+
+    NSNumber *result = @(bsdiffpatch::patchFile(oldFileCString, newFileCString, patchFileCString));
     resolve(result);
 }
 
+RCT_EXPORT_METHOD(diff:(NSString*) oldFile
+                  newFile:(NSString*) newFile
+                  patchFile:(NSString*) patchFile
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+    if (oldFile == nil || oldFile.length == 0 || newFile == nil || newFile.length == 0 || patchFile == nil || patchFile.length == 0) {
+        NSString *message = @"oldFile, newFile, and patchFile should not be nil or empty";
+        reject(@"EINVAL", message, nil);
+        return;
+    }
+    if ([oldFile isEqualToString:newFile] || [oldFile isEqualToString:patchFile] || [newFile isEqualToString:patchFile]) {
+        NSString *message = @"oldFile, newFile, and patchFile should not be the same";
+        reject(@"EINVAL", message, nil);
+        return;
+    }
+
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:oldFile]) {
+        NSString *message = [NSString stringWithFormat:@"oldFile: %@! does not exist", oldFile];
+        reject(@"ENOENT", message, nil);
+        return;
+    }
+    if (![fileManager fileExistsAtPath:newFile]) {
+        NSString *message = [NSString stringWithFormat:@"newFile: %@! does not exist", newFile];
+        reject(@"ENOENT", message, nil);
+        return;
+    }
+    if ([fileManager fileExistsAtPath:patchFile]) {
+        NSString *message = [NSString stringWithFormat:@"patchFile: %@! already exists", newFile];
+        reject(@"ENOENT", message, nil);
+        return;
+    }
+
+    const char *oldFileCString = [oldFile UTF8String];
+    const char *newFileCString = [newFile UTF8String];
+    const char *patchFileCString = [patchFile UTF8String];
+
+    NSNumber *result = @(bsdiffpatch::diffFile(oldFileCString, newFileCString, patchFileCString));
+    resolve(result);
+}
 
 @end
